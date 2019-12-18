e4020afdf6ad32632586cdb649d5dcca79fd987d
Praveen <pvnsripati@gmail.com>
2019-12-18 16:43:27 +0530

    Create a function to POST data

--- website/app.js
+++ website/app.js
@@ -39,6 +39,22 @@ const getWeather = async (baseUrl, zipOrCity, celciusUnit, APIkey) => {
 }
 
 /* Function to POST data */
-
+const postData = async (url='', data={}) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await res.json();
+    return newData;
+  } catch(error) {
+    console.log("error", error);
+    alert("Something went wrong!");
+  }
+}
 
 /* Function to GET Project Data */
