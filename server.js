d4f954b7e976f3a88279d1524e4e8f9cb0539daa
Praveen <pvnsripati@gmail.com>
2019-12-18 16:33:17 +0530

    Create Node environment using express framework

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 // Setup empty JS object to act as endpoint for all routes
+const projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('Cors');
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log('server is running...')
+  console.log(`port is ${port}`);
+}
 
 //Get route
 
