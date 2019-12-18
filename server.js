4f5d1543e005a5f9402dc4f54963a6b64a6ea12f
Praveen <pvnsripati@gmail.com>
2019-12-18 16:28:53 +0530

    Modify UI

---
+++ server.js
@@ -0,0 +1,18 @@
+// Setup empty JS object to act as endpoint for all routes
+
+// Require Express to run server and routes
+
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+
+// Setup Server
+
+//Get route
+
+//Post route
