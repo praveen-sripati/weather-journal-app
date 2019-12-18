f38e040cddfa180e0e59eabf3007fbcfdf3fa708
Praveen <pvnsripati@gmail.com>
2019-12-18 16:38:35 +0530

    Create variables required for forming Web API url

--- website/app.js
+++ website/app.js
@@ -1,4 +1,7 @@
 // Personal API Key for OpenWeatherMap API
+let baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const APIkey = "&appid=113134aff3ecf62594b59dce3068d92e";
+const celciusUnit = "&units=metric";
 
 // Event listener to add function to existing HTML DOM element
 
